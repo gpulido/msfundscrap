da119c5cc2f678e434a1b321827aa4bc103dcab4
gpulido <gabriel.pulidodetorres@gmail.com>
2020-10-20 20:35:15 +0200

    Add rating parsing, and create more helper methods

--- src/scraper.py
+++ src/scraper.py
@@ -1,12 +1,48 @@
 from bs4 import BeautifulSoup
 from dataclasses import dataclass
-import locale
+#import locale
+
+
+def sanitize_text(text):
+    return text.replace('\n', '').strip()
 
 def read_float_with_comma(num):
     return float(num.replace(",", "."))
     #return locale.atof(num)
     
-  
+def number_from_class(number_class):
+    """Helper method to return the last char as integer from a numbered
+    class. example: stars1 stars2 stars3
+
+    Args:
+        number_class (str): class name that has a number at the end
+
+    Returns:
+        int: the number representation of the class.
+    """
+    return int(number_class[-1])
+
+def rating_from_class(rating_class):
+    number = int(rating_class[-1])
+    if number == 1:
+        return "Negative"
+    if number == 2:
+        return "Neutral"
+    if number == 3:
+        return "Bronze"
+    if number == 4:
+        return "Silver"
+    if number == 5:
+        return "Gold"
+    if number == 6:
+        return "Under Review"
+    if number == 7:
+        return "Not Ratable"    
+    return None
+
+
+
+
 @dataclass
 class MSFund:
     """Class to keep information of funds.
@@ -22,18 +58,22 @@ class MSFund:
     common_expenses: int = 0
     Sustainability: int = 1#change to range from 1-5
     sharpe: float = 0.0
+    rating: str = None
     
 
-def parse_table(table, header_class, value_class):
+def parse_table(table, header_class, value_class):    
     """Parses a TR / TD table from MS 
     TODO: move to the helpers
 
     Args:
         table (soup table): Soup table loaded from MS
+        header_class(str): the name of the css class that identifies header columns
+        value_class(str): the name of the css class that identifies value columns
 
     Returns:
         [dictionary]: dictionary with the values of the table
     """
+
     values = {}    
     for row in table.find_all('tr'):
         columns = row.find_all('td')
@@ -58,20 +98,27 @@ def parse_general(soup_page, fund):
         fund (MsFund): The fund to fill with data from the general page
     """
 
-    title = soup_page.title.string.split('|')
-    fund.name = title
-    #find stars variable
-    stars_span = soup_page.findAll("span", {"class": "rating_sprite"})   
-    stars = int(stars_span[0].attrs['class'][1][-1])
-    fund.stars = stars
+    title = soup_page.title.string.split('|')[0]
+    fund.name = sanitize_text(title)
+    
+    rating_span = soup_page.findAll("span", {"class": "rating_sprite"})   
+    #stars
+    stars = rating_span[0].attrs['class'][1]
+    fund.stars =  number_from_class(stars)
+    #rating
+    if len(rating_span) > 1:
+        rating = rating_span[1].attrs['class'][1]
+        fund.rating = rating_from_class(rating)
+
     #search for quickstats
     quickstats = soup_page.findAll("div", {"id": "overviewQuickstatsBenchmarkDiv"})[0].find('table')
     values = parse_table(quickstats, "heading", "text")    
-    #TODO: parse quickstats values into fund    
+    #TODO: parse quickstats values into fund   
     #Sustainability
-    sust_div = soup_page.findAll("div", {"class": "sal_sustainability__score"})
-    sust = int(sust_div[0].attrs['class'][1][-1])
-    fund.Sustainability  = sust   
+    sust_div = soup_page.findAll("div", {"class": "sal-sustainability__score"})
+    sust = number_from_class(sust_div[1].attrs['class'][1])
+    fund.Sustainability  = sust
+   
     
 
 def parse_rating_risk(soup_page, fund):
@@ -92,7 +139,7 @@ def parse_rating_risk(soup_page, fund):
 
 #TODO: Test code to be removed when the proper calls are implemented
 isin = 'F00000UDVS'
-general = BeautifulSoup(open(f'test_pages/{isin}_general.html'), "html.parser")
+general = BeautifulSoup(open(f'test_pages/{isin}_general2.html'), "html.parser")
 rating_riks = BeautifulSoup(open(f'test_pages/{isin}_rating_risk.html'), "html.parser")
 fund = MSFund(isin)
 #print(soup.prettify)
